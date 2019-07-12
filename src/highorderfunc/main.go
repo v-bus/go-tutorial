6f27b2690dbe5569b62e72fad84d54bd83f42f7f
viktor-bushmin <viktor.bushmin@gmail.com>
2019-07-12 14:31:43 +0300

    Update highorderfunc\main.go add closures.SimpleClosureExample() calling

--- src/highorderfunc/main.go
+++ src/highorderfunc/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"highorderfunc/closures"
 	"highorderfunc/funcs"
 )
 
 func main() {
 	funcs.RunHighOrderFuncAttr()
 	funcs.RunHighOrderReturnFunc()
+
+	closures.SimpleClosureExample()
 }
