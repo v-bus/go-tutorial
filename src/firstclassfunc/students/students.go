83135329b6be956f928de446b08cbcad59c45ae7
viktor-bushmin <viktor.bushmin@gmail.com>
2019-07-12 15:16:08 +0300

    Update firstclassfunc\students\students.go added filter to RunStudentFilter() find all students from Russia added third student in example

--- src/firstclassfunc/students/students.go
+++ src/firstclassfunc/students/students.go
@@ -35,7 +35,13 @@ func RunStudentFilter() {
 		grade:     "B",
 		country:   "USA",
 	}
-	s := []student{s1, s2}
+	s3 := student{
+		firstName: "Viktor",
+		lastName:  "Bushmin",
+		grade:     "B",
+		country:   "Russia",
+	}
+	s := []student{s1, s2, s3}
 	resS := filter(s, func(stdnt student) bool {
 		if stdnt.grade == "B" {
 			return true
@@ -47,4 +53,12 @@ func RunStudentFilter() {
 
 	fmt.Println("filtered slice  of students resS is ", resS)
 
+	//we want to find all students from Russia
+	c := filter(s, func(students student) bool {
+		if students.country == "Russia" {
+			return true
+		}
+		return false
+	})
+	fmt.Println("filtered slice  of students c is ", c)
 }
