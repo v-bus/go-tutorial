1eb21fad1a3e48a6d13847c38abd02f4ea6efc01
viktor-bushmin <viktor.bushmin@gmail.com>
2019-07-14 23:16:42 +0300

    Update \src\filehandling\filehandling.go add openTestFileByBufio3Bytes() which lunch code of example with reading bufio.reader from bufio package file by buffer of 3 bytes using fixed bug whith double init of the flag "fpath"

--- src/filehandling/filehandling.go
+++ src/filehandling/filehandling.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"os"
 	"path/filepath"
 	"tryerrors/filepatherrors"
@@ -20,6 +22,7 @@ func openTestFileByName() {
 
 		return
 	}
+	fmt.Println("openTestFileByName()")
 	fmt.Println("File name: ", "test.txt")
 	fmt.Println("File data: ", string(data))
 }
@@ -27,9 +30,10 @@ func getMultArg(args ...interface{}) []interface{} {
 	return args
 }
 func openTestFileAbsoluteFilePath() {
+	fmt.Println("openTestFileAbsoluteFilePath")
 	fileName := fmt.Sprint(
 		getMultArg(os.LookupEnv("GOPATH"))[0],
-
+		string(filepath.Separator),
 		"src",
 		string(filepath.Separator),
 		"filehandling",
@@ -48,6 +52,7 @@ func openTestFileAbsoluteFilePath() {
 }
 
 func openTestFileByArgFlag() {
+	fmt.Println("openTestFileByArgFlag")
 	fptr := flag.String("fpath", "test.txt", "file path to read file")
 	flag.Parse()
 	fileName := *fptr
@@ -64,14 +69,54 @@ func openTestFileByArgFlag() {
 }
 
 func openTestFileByPackrBox() {
-
-	box := packr.NewBox("..\filehandling")
+	fmt.Println("openTestFileByPackrBox")
+	box := packr.NewBox("../filehandling")
 	data := box.String("test.txt")
 	fmt.Println("Content of test.txt:", data)
+}
+
+func openTestFileByBufio3Bytes() {
+	fmt.Println("openTestFileByBufio3Bytes")
+	var fptr *string //flag string argument pointer
+	defer func() {
+		if r := recover(); r != nil {
+			log.Fatal(r)
+		}
+	}()
+	if flag.Lookup("fpath") == nil {
+		fptr = flag.String("fpath", "test.txt", "open test.txt file by 3 bytes")
+	} else {
+		fstr := flag.Lookup("fpath").Value.String()
+		fptr = &fstr
+		fmt.Println(*fptr)
+	}
+	flag.Parse() //do after flags parameters set
+
+	fh, err := os.Open(*fptr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer func() {
+		if err = fh.Close(); err != nil {
+			log.Fatal(err)
+		}
+	}()
+	r := bufio.NewReader(fh)
+	
+	for  {
+		b := make([]byte, 3)
+		_, err = r.Read(b)
+		if err!=nil{
+			fmt.Println("Error reading file:",err)
+			break
+		}
+		fmt.Println(string(b))
+	}
 }
 func main() {
 	openTestFileByName()
 	openTestFileAbsoluteFilePath()
 	openTestFileByArgFlag()
 	openTestFileByPackrBox()
+	openTestFileByBufio3Bytes()
 }
