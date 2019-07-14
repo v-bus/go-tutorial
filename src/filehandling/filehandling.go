c3593ada66c2dd94077a46a960d745bbea01ea71
viktor-bushmin <viktor.bushmin@gmail.com>
2019-07-14 11:07:21 +0300

    Update src\filehandling\filehandling.go added openTestFileAbsoluteFilePath with feature create fileName from GOPATH+diff to test.txt file using filepath.Separator

--- src/filehandling/filehandling.go
+++ src/filehandling/filehandling.go
@@ -1,12 +1,14 @@
 package main
 
 import (
-	"tryerrors/filepatherrors"
 	"fmt"
 	"io/ioutil"
+	"os"
+	"path/filepath"
+	"tryerrors/filepatherrors"
 )
 
-func main() {
+func openTestFileByName() {
 	data, err := ioutil.ReadFile("test.txt")
 	if err != nil {
 		fmt.Println("File text.txt was not read")
@@ -16,4 +18,30 @@ func main() {
 		return
 	}
 	fmt.Println("File data: ", string(data))
+}
+func getMultArg(args ...interface{}) []interface{} {
+	return args
+}
+func openTestFileAbsoluteFilePath() {
+	fileName := fmt.Sprint(
+		getMultArg(os.LookupEnv("GOPATH"))[0],
+		string(filepath.Separator),
+		"src",
+		string(filepath.Separator),
+		"filehandling",
+		string(filepath.Separator),
+		"test.txt")
+	data, err := ioutil.ReadFile(fileName)
+	if err != nil {
+		fmt.Println("File", fileName, " was not read")
+		fmt.Println(filepatherrors.ReturnNames(fileName))
+		fmt.Println(filepatherrors.ReturnNames("*"))
+
+		return
+	}
+	fmt.Println("File data: ", string(data))
+}
+func main() {
+	openTestFileByName()
+	openTestFileAbsoluteFilePath()
 }
