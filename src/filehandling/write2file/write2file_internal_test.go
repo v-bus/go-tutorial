57035c621be7c562c36d08ad054a7d80204ecf33
viktor-bushmin <viktor.bushmin@gmail.com>
2019-07-17 22:11:05 +0300

    Update src\filehandling\write2file\write2file_internal_test.go added sub-tests to TestCheckAllAttrNotEmpty

--- src/filehandling/write2file/write2file_internal_test.go
+++ src/filehandling/write2file/write2file_internal_test.go
@@ -21,4 +21,20 @@ func TestCheckAllAttrNotEmpty(t *testing.T) {
 				t.Error("checkAllAttrNotEmpty(\"parameter\", 1, 2.2, true)== true - FAILED")
 			}
 		})
+		t.Run("3rd",
+		func(t *testing.T) {
+			if checkAllAttrNotEmpty("","")== false {
+				t.Log("checkAllAttrNotEmpty(\"\",\"\")== false - OK")
+			} else {
+				t.Error("checkAllAttrNotEmpty(\"\",\"\")== false - FAILED")
+			}
+		})
+		t.Run("4th",
+		func(t *testing.T) {
+			if checkAllAttrNotEmpty("Hello world","C:\\Users\\user1\\Documents\\go\\go-tutorial\\src\filehandling\\write2file\\write2file_internal_test.go")== true {
+				t.Log("checkAllAttrNotEmpty(\"Hello world\",\"C:\\Users\\user1\\Documents\\go\\go-tutorial\\src\filehandling\\write2file\\write2file_internal_test.go\")== true - OK")
+			} else {
+				t.Error("checkAllAttrNotEmpty(\"Hello world\",\"C:\\Users\\user1\\Documents\\go\\go-tutorial\\src\filehandling\\write2file\\write2file_internal_test.go\")== true - FAILED")
+			}
+		})
 }
